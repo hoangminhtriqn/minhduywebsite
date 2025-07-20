550cca1754ae1c5cf260a07ae26192b770e114a3
Timo <huynhvanthuy97@gmail.com>
2025-07-20 23:23:30 +0700

    sửa title tab icon

--- fe/src/components/FaviconManager.tsx
+++ fe/src/components/FaviconManager.tsx
@@ -6,11 +6,12 @@ const FaviconManager: React.FC = () => {
     const existingLinks = document.querySelectorAll("link[rel*='icon']");
     existingLinks.forEach((link) => link.remove());
 
-    // Create new favicon link
+    // Create new favicon link with proper attributes
     const link = document.createElement("link");
     link.rel = "icon";
-    link.type = "image/x-icon";
-    link.href = "/favicon.ico";
+    link.type = "image/png";
+    link.href = "/images/logo.png";
+    link.sizes = "16x16 32x32";
 
     // Add to head
     document.head.appendChild(link);
@@ -18,9 +19,22 @@ const FaviconManager: React.FC = () => {
     // Also add shortcut icon
     const shortcutLink = document.createElement("link");
     shortcutLink.rel = "shortcut icon";
-    shortcutLink.type = "image/x-icon";
-    shortcutLink.href = "/favicon.ico";
+    shortcutLink.type = "image/png";
+    shortcutLink.href = "/images/logo.png";
     document.head.appendChild(shortcutLink);
+
+    // Add apple touch icon
+    const appleTouchLink = document.createElement("link");
+    appleTouchLink.rel = "apple-touch-icon";
+    appleTouchLink.href = "/images/logo.png";
+    document.head.appendChild(appleTouchLink);
+
+    // Force browser to reload favicon
+    const link2 = document.createElement("link");
+    link2.rel = "icon";
+    link2.type = "image/png";
+    link2.href = "/images/logo.png?" + new Date().getTime();
+    document.head.appendChild(link2);
   }, []);
 
   return null;
